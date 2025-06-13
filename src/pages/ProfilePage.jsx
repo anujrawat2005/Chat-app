4a561c0277523a61852696c77327a26b55abe253
anujrawat2005 <anujrawat724@gmail.com>
2025-06-13 22:33:31 +0530

    part-backend

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -8,13 +8,23 @@ const ProfilePage = () => {
   const navigate = useNavigate();
   const[name,setName] = useState("Martin Johnson")
   const [bio,setBio] = useState("Hi everyone i am using chat-app")
+
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    navigate('/')
+  }
+
+
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center
     justify-center">
       
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600
         flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
-        <form className="flex flex-col gap-5 p-10 flex-1">
+
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
           <h3 className=" text-lg">
             Profile Details
           </h3>
@@ -26,9 +36,18 @@ const ProfilePage = () => {
              className={`w-12 h-12 ${ selectedImage && "rounded-full"}`} alt=''/>
             Upload profile Image
           </label>
+          <input type="text"  required placeholder="Your Name" className="p-2 border border-gray-500 rounded-md 
+          focus:outline-none focus:ring-2 focus-ring-violet-500"    />
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+            placeholder="write profile bio" required className="p-2 border border-gray-500 
+            rounded-md focus:outline-none focus:ring-2
+            focus:ring-violet-500" rows={4}></textarea>
+
+            <button type="Submit" className="bg-gradient-to-r from-purple-400 to-violet-500
+            text-white p-2 rounded-full text-lg cursor-pointer">Save</button>
 
         </form>
-        <img src=''alt=''/>
+        <img className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" src={assets.logo_icon} alt=''/>
       </div>
     </div>
   )
